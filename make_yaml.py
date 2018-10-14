26905df1bd193c09e91f14115b538ff6812a28a3
tr <sketchbang@gmail.com>
2018-10-13 21:38:06 -0500

    tweak image balancing algo

--- make_yaml.py
+++ make_yaml.py
@@ -2,6 +2,10 @@ import sys
 import os
 import glob
 from PIL import Image
+import toolz
+import random
+
+COLUMNS = 3
 
 if len(sys.argv) is not 2:
     print("Must give path")
@@ -26,12 +30,41 @@ for img_fname in os.listdir(directory):
       description:
 
     """
-    items.append(item_string)
+    items.append({
+        'rel_height': h / w,
+        'yaml': item_string
+    })
+
+items_reverse_sorted = sorted(items, key=lambda x: x['rel_height'], reverse=True)
+min_group = 0
+
+def sum_group(xs):
+    return sum([x['rel_height'] for x in xs])
+
+groups = [list() for _ in range(COLUMNS)]
+
+for i in items_reverse_sorted:
+    min_group = groups[0]
+    min_sum = sum_group(min_group)
+
+    for g in groups[1:]:
+        current_sum = sum_group(g)
+        if current_sum <= min_sum:
+            min_group = g
+    min_group.append(i)
+
+# shuffle for looks, so that we don't get a blocky grid
+for g in groups:
+    random.shuffle(g)
+
+# now stagger them, so they will be doled out into columns
+items = toolz.interleave(groups)
+
+yaml_strings = map(lambda x: x['yaml'], items)
 
 yaml = f"""{yaml}
-image_dir: {path}
 images:
-    {"".join(items)}
+    {"".join(yaml_strings)}
 ---
 """
 
